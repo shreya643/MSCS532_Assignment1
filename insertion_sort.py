dfeae6af3b1a9fc480491bee72cbdd2dded2554e
shreya643 <shreya998.ss@gmail.com>
2024-10-24 21:53:00 -0500

    adding insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+    #loop over the array
+    for i in range(1, len(arr)):
+        key = arr[i]
+        
+        j = i - 1
+        #while loop to sort array in desc order
+        while j >= 0 and key > arr[j]: 
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+if __name__ == "__main__":
+    arr = [12, 40, 60, 22, 11, 13, 5, 6]
+    print("Input:", arr)
+    insertion_sort(arr)
+    print("Output: ", arr)
